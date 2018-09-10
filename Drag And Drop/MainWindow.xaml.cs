68d6510fc6b256e86fe9164aaf1130c623e876a9
Ho <v.ho@fugro.com>
2018-09-10 17:43:36 +0800

    Initial Commit

---
+++ Drag And Drop/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Drag_And_Drop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            SizeChanged += (o, e) =>
+            {
+                var r = SystemParameters.WorkArea;
+                Left = r.Right - ActualWidth;
+                Top = r.Bottom - ActualHeight;
+            };
+            InitializeComponent();
+            this.SourceInitialized += Window1_SourceInitialized;
+            this.DataContext = this;
+
+            var listbox = DropBox;
+            ListBoxItem itm = new ListBoxItem();
+            itm.Content = "Drag file to here.";
+            listbox.Items.Add(itm);
+
+        }
+
+        private void DropBox_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Copy;
+                var listbox = sender as ListBox;
+                listbox.Background = new SolidColorBrush(Color.FromRgb(155, 155, 155));
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void DropBox_DragLeave(object sender, DragEventArgs e)
+        {
+            var listbox = sender as ListBox;
+            listbox.Background = new SolidColorBrush(Color.FromRgb(226, 226, 226));
+        }
+
+        private void DropBox_Drop(object sender, DragEventArgs e)
+        {
+            var listbox = sender as ListBox;
+            listbox.Background = new SolidColorBrush(Color.FromRgb(226, 226, 226));
+            
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+           {
+                listbox.Items.Clear();
+
+                String[] files = (String[])e.Data.GetData(DataFormats.FileDrop);
+                for (int i = 0; i < files.Length; i++)
+                {
+                    if(i == 0)
+                    {
+                        ListBoxItem itemFirstLine = new ListBoxItem();
+                        itemFirstLine.Content = files.Length + " files have been added into Paperless System.";
+                        listbox.Items.Add(itemFirstLine);
+                    }
+
+                    ListBoxItem item = new ListBoxItem();
+                    item.Content = i+1 + ". " + System.IO.Path.GetFileName(files[i]);
+                    listbox.Items.Add(item);
+                }
+           }
+        }
+
+        private void Window1_SourceInitialized(object sender, EventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            HwndSource source = HwndSource.FromHwnd(helper.Handle);
+            source.AddHook(WndProc);
+        }
+
+        const int WM_SYSCOMMAND = 0x0112;
+        const int SC_MOVE = 0xF010;
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+
+            switch (msg)
+            {
+                case WM_SYSCOMMAND:
+                    int command = wParam.ToInt32() & 0xfff0;
+                    if (command == SC_MOVE)
+                    {
+                        handled = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+    }
+}
